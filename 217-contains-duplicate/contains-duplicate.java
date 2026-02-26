c5d08392f55774bba4811d3d9422f3f7456d2e1a
Aadya2901 <145851414+Aadya2901@users.noreply.github.com>
2026-02-26 11:31:52 +0530

    Time: 22 ms (32.61%) | Memory: 81.1 MB (71.96%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,12 +1,13 @@
-class Solution {
-  public boolean containsDuplicate(int[] nums) {
-    Arrays.sort(nums);
+import java.util.HashSet;
 
-    for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                return true;
-            }
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+        return set.size() != nums.length;
     }
-    return false;
-  }
 }
